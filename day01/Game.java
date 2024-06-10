f0a45d817e988d5ab3bc4a65f11c8f5b678f37d2
jjunik <tjwnsdlr4545@gmail.com>
2024-06-10 21:53:44 +0900

    Add files via upload

---
+++ day01/Game.java
@@ -0,0 +1,4 @@
+package java2.day01;
+
+public class Game {
+}
---
+++ day01/Review.java
@@ -0,0 +1,17 @@
+package java2.day01;
+
+import java.util.Scanner;
+
+public class Review {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("----바탕화면----");
+        System.out.println("1.피파온라인");
+        System.out.println("2.메이플스토리");
+        System.out.print("선택: ");
+        sc.nextInt();
+
+    }
+
+}
