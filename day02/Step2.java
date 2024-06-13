135f9bf431fc04038f5999e607af99dcf6eb580a
jjunik <tjwnsdlr4545@gmail.com>
2024-06-13 18:18:02 +0900

    Update Step2.java

--- day02/Step2.java
+++ day02/Step2.java
@@ -1,7 +1,7 @@
 package day02;
 
 public class Step2 {
-    // 함수명() throw 예외클래스{}: 예외 던지기, 어디로: 해당 함수를 호출했던 곳으로
+    // 함수명() throws 예외클래스{}: 예외 던지기, 어디로: 해당 함수를 호출했던 곳으로
     static void findClass() throws ClassNotFoundException {
         Class.forName("java.lang.String2");
     }
